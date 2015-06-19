86d7e10d3c1ea9578d475b6fcd7cda6765bbb96d
fan-t-fukuoka <t_fukuoka@fancs.com>
2015-06-19 12:19:27 +0900

    Remove extra files.

--- CocosNendSample/proj.android/obj/local/armeabi/objs-debug/cocostudio_static/CCArmatureDefine.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs-debug/cocostudio_static/CCArmatureDefine.o: \
- /Users/t_fukuoka/cocos2dx/cocos2d-x-3.6/projects/CocosNendSample/proj.android/../cocos2d/cocos/editor-support/cocostudio/CCArmatureDefine.cpp \
- /Users/t_fukuoka/cocos2dx/cocos2d-x-3.6/projects/CocosNendSample/proj.android/../cocos2d/cocos/editor-support/cocostudio/../cocostudio/CCArmatureDefine.h
-
-/Users/t_fukuoka/cocos2dx/cocos2d-x-3.6/projects/CocosNendSample/proj.android/../cocos2d/cocos/editor-support/cocostudio/../cocostudio/CCArmatureDefine.h:
