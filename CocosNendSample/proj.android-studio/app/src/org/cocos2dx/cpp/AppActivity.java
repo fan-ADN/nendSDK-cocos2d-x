f66089d2a91b124b575666ee1c7b12de8c837660
fan-t-fukuoka <t_fukuoka@fancs.com>
2015-10-16 10:09:11 +0900

    Updated sample.

--- CocosNendSample/proj.android-studio/app/src/org/cocos2dx/cpp/AppActivity.java
+++ CocosNendSample/proj.android-studio/app/src/org/cocos2dx/cpp/AppActivity.java
@@ -1,26 +1,26 @@
 /****************************************************************************
-Copyright (c) 2015 Chukong Technologies Inc.
- 
-http://www.cocos2d-x.org
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
+ * Copyright (c) 2015 Chukong Technologies Inc.
+ * <p/>
+ * http://www.cocos2d-x.org
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ****************************************************************************/
 package org.cocos2dx.cpp;
 
 import android.content.Context;
@@ -37,7 +37,7 @@ public class AppActivity extends Cocos2dxActivity {
     private static boolean backKeySelected;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         sContext = this;
@@ -47,6 +47,7 @@ public class AppActivity extends Cocos2dxActivity {
     public static boolean isBackKeySelected() {
         return backKeySelected;
     }
+
     public static void setBackKeySelected(boolean backKeySelected) {
         AppActivity.backKeySelected = backKeySelected;
     }
@@ -60,23 +61,23 @@ public class AppActivity extends Cocos2dxActivity {
 
         if (event.getAction() == KeyEvent.ACTION_DOWN) {
 
-            if (event.getKeyCode() == KeyEvent.KEYCODE_BACK){
+            if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
 
                 setBackKeySelected(true);
-            }else{
+            } else {
                 setBackKeySelected(false);
             }
-        }else if (event.getAction() == KeyEvent.ACTION_UP){
+        } else if (event.getAction() == KeyEvent.ACTION_UP) {
 
-            if (event.getKeyCode() == KeyEvent.KEYCODE_BACK){
+            if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
                 setBackKeySelected(true);
                 // 終了時広告の表示処理を行う
                 NendInterstitialModule.showNADInterstitialViewFromBackKey();
-            }else{
+            } else {
                 setBackKeySelected(false);
             }
 
-        }else{
+        } else {
             setBackKeySelected(false);
         }
 
