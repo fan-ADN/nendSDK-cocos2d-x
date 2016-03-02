17eacbe6f08b18146a8b5d045a82d45b7a8423dc
fan-t-fukuoka <t_fukuoka@fancs.com>
2016-03-02 17:25:51 +0900

    Removed ignore files.

--- CocosNendSample/proj.android/obj/local/armeabi/objs-debug/recast_static/fastlz/fastlz.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs-debug/recast_static/fastlz/fastlz.o: \
- /Users/t_fukuoka/github_adn/nendSDK-cocos2d-x/CocosNendSample/proj.android/../cocos2d/external/recast/fastlz/fastlz.c \
- /Users/t_fukuoka/github_adn/nendSDK-cocos2d-x/CocosNendSample/proj.android/../cocos2d/external/recast/fastlz/fastlz.c
-
-/Users/t_fukuoka/github_adn/nendSDK-cocos2d-x/CocosNendSample/proj.android/../cocos2d/external/recast/fastlz/fastlz.c:
