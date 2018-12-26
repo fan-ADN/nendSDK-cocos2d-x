3d0388dbb48e4b9aaecc4c0af6d0d9f09def8961
fan-t-kinami <t_kinami@fancs.com>
2018-12-26 12:54:52 +0900

    Updated sample : using setMuteStartPlaying

--- CocosNendSample/Classes/VideoAdScene.cpp
+++ CocosNendSample/Classes/VideoAdScene.cpp
@@ -170,6 +170,9 @@ bool VideoAdScene::init()
     _interstitialVideoAd->addFallbackFullBoard("485504", "30fda4b3386e793a14b27bedb4dcd29f03d638e5");
 #endif
     
+    // Set mute state to play video
+    _interstitialVideoAd->setMuteStartPlaying(false);
+    
     // Regist interstitialVideoAd callbacks
     _interstitialVideoAd->onLoaded([=](const VideoAds::InterstitialVideoAd& ad) {
         Director::getInstance()->getScheduler()->performFunctionInCocosThread([=](){
